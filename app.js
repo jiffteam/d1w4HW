d3366af1c0f17a0bbfa4a172750bc7b433269280
kameron <kamscott3@gmail.com>
2022-04-18 22:02:24 -0400

    Hw1

---
+++ app.js
@@ -0,0 +1,34 @@
+//setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 11; i++){
+    myArray.push(i);
+}
+
+
+//setup
+    const myArray = [];
+
+// Only change code below this line
+    let i = 10;
+while(i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 11; i < 21; i += 2){
+  myArray.push(i);
+}
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 15; i > 0; i -= 2){
+  myArray.push(i);
+}
